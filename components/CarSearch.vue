c2a41d6ddbcdedd3deab31b76b49bbdc5b6854d2
ymmy1 <chem-pion@mail.ru>
2019-09-03 00:57:49 -0700

    APIs page 90% done

--- components/CarSearch.vue
+++ components/CarSearch.vue
@@ -1,25 +1,66 @@
+<style lang="scss" scoped>
+#accordion {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: baseline;
+
+  .card {
+    display: inline-block;
+    width: 23%;
+
+    a {
+      img {
+        width: 100%;
+      }
+    }
+
+  }
+}
+
+.modal-content {
+  a {
+    // display: block;
+    width: 100%;
+    color: #ffffff;
+    text-decoration: none;
+
+    button {
+      width: 100%;
+    }
+  }
+}
+
+</style>
 <template>
   <div id="accordion">
     <div v-for="(poster, index) in cars" :key="index" class="card">
       <div class="card-header">
         <a v-bind:href="'#' + poster.div_id" data-parent="#accordion" data-toggle="collapse">
           <img v-bind:src="poster.img_link" alt="">
+          <h3>{{poster.car_name}}</h3>
+          <p><span class="font-weight-bold">MSRP: </span>{{poster.msrp}}</p>
         </a>
       </div>
       <div v-bind:id="poster.div_id" class="collapse">
         <div class="card-body">
-          <h3 class="display-4" data-toggle="modal" v-bind:data-target="'.' + poster.div_class">{{poster.movie_name}}</h3>
+          <div>
+            <p><span class="font-weight-bold">Range: </span>{{poster.range}}</p>
+            <p><span class="font-weight-bold">MSRP: </span>{{poster.msrp}}</p>
+            <p><span class="font-weight-bold">MPGe: </span>{{poster.mpge}}</p>
+            <p><span class="font-weight-bold">Battery charge life: </span>{{poster.charge}}</p>
+            <p><span class="font-weight-bold">Battery: </span>{{poster.battery}}</p>
+            <p><a :href="poster.rate_1_link">{{poster.rate_1}}</a></p>
+            <p><a :href="poster.rate_2_link">{{poster.rate_2}}</a></p>
+            <div data-toggle="modal" v-bind:data-target="'.' + poster.div_class">
+              <button class="btn-primary">Watch more>></button>
+            </div>
+          </div>
           <div v-bind:class="'modal fade ' + poster.div_class" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg">
               <div class="modal-content">
-                {{poster.modal_line_1}}<br>
-                {{poster.modal_line_2}}<br>
-                {{poster.modal_line_3}}<br>
-                {{poster.modal_line_4}}<br>
-                {{poster.modal_line_5}}<br>
-                {{poster.modal_line_6}}<br>
-                {{poster.modal_line_7}}<br>
-                <a :href="poster.weblink">google</a>
+                <iframe :src="poster.youtube" allowfullscreen height="500px"></iframe>
+                <a :href="poster.weblink" target="_blank"><button class="btn-primary">Order from original website</button></a>
               </div>
             </div>
           </div>
@@ -41,44 +82,3 @@ export default {
 }
 
 </script>
-<style lang="scss" scoped>
-#accordion {
-  display: inline-block;
-  display: flex;
-  justify-content: space-around;
-  padding: 0 0 2%;
-  align-items: baseline;
-
-  img {
-    width: 400px;
-    height: 550px;
-    object-fit: cover;
-  }
-
-  .card-body {
-    padding: 0;
-  }
-
-  // Styling modal button
-  h3 {
-    padding: 2% 0;
-    font-weight: bold;
-    margin: 0;
-    cursor: pointer;
-    text-align: center;
-    font-size: 2.3em;
-    transition: 0.5s;
-
-    &:hover {
-      background: lightblue;
-      color: white;
-    }
-  }
-}
-
-.modal-content {
-  font-size: 2em;
-  padding: 5% 10%;
-}
-
-</style>
